f594701ded7e5667aec1aef5890b3a46fc32a0f5
SAndrade100 <andradedearaujo100@gmail.com>
2024-01-01 09:45:33 -0300

    Tests with TLS server stopped, routing with express started, Teacher class was changed, TeacherList added.

--- server.js
+++ server.js
@@ -11,53 +11,67 @@
 // }).listen(1337);
 
 // This is a test with express
-// const express = require('express');
-// const app = express();
-// const port = 3000;
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 8080;
+
+const {Teacher, TeacherList} = require('./src/Teacher');
+const {join} = require("path");
+
+app.get('/', function(request, res){
+    const pathFile = join(__dirname, 'public', 'index.html');
+    res.sendFile(pathFile);
+});
+
+app.listen(port, function () {
+    console.log('Server running at port: http://localhost:' + port);
+});
+
+const teacherList = new TeacherList();
+
+app.get('/add-teacher', function (req, res) {
+    const {name, birth, graduation} = req.body;
+    const teacher = new Teacher(name, birth, graduation);
+
+    teacherList.addTeacher(teacher);
+    teacherList.saveData();
+})
+
+// 'use strict'
 //
-// app.get('/', function(request, response){
-//     response.send('Hello, this is a text!');
+// const tls = require('tls');
+// const fs = require('fs');
+// const path = require("path");
+// const qs = require('querystring');
+//
+// const PORT = 443;
+// const HOST = '127.0.0.1';
+//
+// let options = {
+//     key: fs.readFileSync(path.join(__dirname, 'ssl-keys', 'private-key.pem')),
+//     cert: fs.readFileSync(path.join(__dirname, 'ssl-keys', 'public-cert.pem'))
+// }
+//
+// let server = tls.createServer(options, function(socket) {
+//     socket.write('I am the server sending you an message.');
+//
+//     socket.on('data', function(data) {
+//         console.log('Received: %s [it is %d bytes long]',
+//             data.toString().replace(/(\n)/gm, ""),
+//             data.length
+//         );
+//     });
+//
+//     socket.on('end', function () {
+//         console.log('End of transmition.');
+//     });
 // });
 //
-// app.listen(port, function () {
-//     console.log('Server running at port: http://localhost:' + port);
-// })
-'use strict'
-
-const tls = require('tls');
-const fs = require('fs');
-const path = require("path");
-
-const PORT = 1337;
-const HOST = '127.0.0.1';
-
-let options = {
-    key: fs.readFileSync(path.join(__dirname, 'ssl-keys', 'private-key.pem')),
-    cert: fs.readFileSync(path.join(__dirname, 'ssl-keys', 'public-cert.pem'))
-}
-
-let server = tls.createServer(options, function(socket) {
-    socket.write('I am the server sending you an message.');
-
-    socket.on('data', function(data) {
-        console.log('Received: %s [it is %d bytes long]',
-            data.toString().replace(/(\n)/gm, ""),
-            data.length
-        );
-    });
-
-    socket.on('end', function () {
-        console.log('End of transmition.');
-    });
-});
-
-server.listen(PORT, HOST, function() {
-    console.log('Listening at %s, on port %s', HOST, PORT);
-});
-
-server.on('error', function(error) {
-    console.log(error);
-    server.destroy();
-});
-
-
+// server.listen(PORT, HOST, function() {
+//     console.log('Listening at %s, on port %s', HOST, PORT);
+// });
+//
+// server.on('error', function(error) {
+//     console.log(error);
+//     server.destroy();
+// });
--- src/Teacher.js
+++ src/Teacher.js
@@ -18,5 +18,35 @@ class Teacher {
         this.days.add(day);
     }
 
-
+    getInfo() {
+        return {
+            id: this.id,
+            name: this.name,
+            birth: this.birth,
+            graduation: this.graduation,
+            subjects: this.subjects,
+            days: this.days
+        };
+    }
 }
+
+class TeacherList {
+    constructor() {
+        this.teacherList = [];
+    }
+
+    addTeacher(Teacher) {
+        this.teacherList.push(Teacher);
+    }
+
+    getTeacher() {
+        return this.teacherList.map(teacher => teacher.getInfo());
+    }
+
+    saveData() {
+        const jsonData = JSON.stringify(this.getTeacher(), null, 2);
+        fs.writeFileSync('teacher.json', jsonData, 'utf-8');
+    }
+}
+
+module.exports = {Teacher, TeacherList};
